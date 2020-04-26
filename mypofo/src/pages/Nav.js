f792ec5e3dca7d9a3bcf45216b6a818f9f553dd2
dev-93 <cjb940103@gmail.com>
2020-04-27 00:46:32 +0900

    nav에서 눌렀을 때, 각 컴포넌트만 렌더링

--- mypofo/src/pages/Nav.js
+++ mypofo/src/pages/Nav.js
@@ -11,63 +11,50 @@ const Nav = () => {
     setAbout(true);
     setPortfolio(false);
     setThink(false);
-
-    return (
-      <>
-        {
-          isAbout && <h1>렌더링 테스트</h1>
-        }
-      </>
-    )
   }
 
   const Port = () => {
     setAbout(false);
     setPortfolio(true);
     setThink(false);
-    
-    return (
-      <>
-        {
-          isPortfolio && <Portfolio />
-        }
-      </>
-    )
   }
 
   const Think = () => {
     setAbout(false);
     setPortfolio(false);
     setThink(true);
-    
-    return (
-      <>
-        {
-          isThink && <h1>world</h1>        
-        }
-      </>
-    )
   }
 
   return (
-    <header className="App-header">
-      <figure>
-        TN
-      </figure>
-      <ul>
-        <li>
-          <button type="button" onClick={About}>About</button>
-        </li>
+    <>
+      <header className="App-header">
+        <figure>
+          TN
+        </figure>
+        <ul>
+          <li>
+            <button type="button" onClick={About}>About</button>
+          </li>
 
-        <li>
-          <button type="button" onClick={Port}>Portfolio</button>
-        </li>
+          <li>
+            <button type="button" onClick={Port}>Portfolio</button>
+          </li>
 
-        <li>
-          <button type="button" onClick={Think}>Think</button>
-        </li>
-      </ul>
-    </header>
+          <li>
+            <button type="button" onClick={Think}>Think</button>
+          </li>
+        </ul>
+      </header>
+      {
+        isAbout && <h1>렌더링 테스트</h1>
+      }
+      {
+        isPortfolio && <Portfolio />
+      }
+      {
+        isThink && <h1>테스트 중</h1>        
+      }
+    </>
   );
 }
 
