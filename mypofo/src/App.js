3d195df4aa90398777d62d4ef3f245e0a46e5657
dev-93 <cjb940103@gmail.com>
2020-05-01 22:12:45 +0900

    메인 컴포넌트 이후 nav와 about 컴포넌트 렌더링

--- mypofo/src/App.js
+++ mypofo/src/App.js
@@ -1,21 +1,71 @@
 import React, {useState} from 'react';
-import Portfolio from "./pages/Portfolio";
+import About from "./pages/about/About";
+import Portfolio from "./pages/portfolio/Portfolio";
+import Timeline from "./pages/timeline/Timeline";
 import Nav from "./components/Nav";
-import Main from "./pages/Main";
+import Main from "./pages/main/Main";
 import './App.scss';
 
 function App() {
+  const [isMain, setMain] = useState(true);
   const [isUpload, setUpload] = useState(false);
-
-  const show = () => {
-    setUpload(true);
+  const [isAbout, setAbout] = useState(false);
+  const [isPortfolio, setPortfolio] = useState(false);
+  const [isThink, setThink] = useState(false);
+ 
+  window.onload = function(){ 
+    setTimeout(function() {
+      setMain(false);
+      setUpload(true);
+      setAbout(true);
+      console.log(100);
+    }, 5000);
   }
+  
+  const about = () => {
+    setAbout(true);
+    setPortfolio(false);
+    setThink(false);
+    console.log(200);
+  }
+
+  const port = () => {
+    setAbout(false);
+    setPortfolio(true);
+    setThink(false);
+    console.log(300);
+  }
+
+  const think = () => {
+    setAbout(false);
+    setPortfolio(false);
+    setThink(true);
+    console.log(400);
+  }
+
   return (
     <>
-      <Main />
+      {
+        isMain && <Main />
+      }
       <div>
         {
-          isUpload && <Nav />
+          isUpload && (
+            <Nav 
+              about={about}
+              port={port}
+              think={think}
+            />
+          ) 
+        }
+        {
+          isAbout && <About />
+        }
+        {
+          isPortfolio && <Portfolio />
+        }
+        {
+          isThink && <Timeline />
         }
       </div>
     </>
