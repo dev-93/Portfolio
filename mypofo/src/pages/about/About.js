3543dc3a260c70d6601285e2ad39055d68a9cac2
dev-93 <cjb940103@gmail.com>
2020-05-01 22:09:56 +0900

    about 폴더 생성 이후 정리

---
+++ mypofo/src/pages/about/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./about.scss"
+
+const About = () => {
+  return (
+    <>
+      <div className="about">
+        <h1>about</h1>
+      </div>
+    </>
+  );
+}
+
+export default About;
