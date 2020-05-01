6a26a975a4036bce1fceb01a4a9a3278d3bf618f
dev-93 <cjb940103@gmail.com>
2020-05-01 22:10:56 +0900

    timeline 폴더 생성 이후 정리

---
+++ mypofo/src/pages/timeline/Timeline.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./timeline.scss"
+
+const Timeline = () => {
+  return (
+    <>
+      <div className="timeline">
+        zz
+      </div>
+    </>
+  );
+}
+
+export default Timeline;
