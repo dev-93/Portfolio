c4f2b23f80a7dceef3e8ff65bba4244a46431135
dev-93 <cjb940103@gmail.com>
2020-05-14 01:54:03 +0900

    about 수정

--- mypofo/src/pages/about/About.js
+++ mypofo/src/pages/about/About.js
@@ -6,8 +6,24 @@ const About = () => {
     <>
       <div className="about">
         <div className="wrapper">
-          <div className="card">
-            
+          <div className="card info">
+            <div className="me">Great Partner</div>
+            <div className="occupation">Front-End Developer</div>
+            <div className="details">
+              <div className="container">
+                <p>cjb940103@gmail.com</p>
+                <p>010-7379-1902</p>
+                <p>Guro-gu, Seoul</p>
+                <a href="https://github.com/dev-93" target="_blank">https://github.com/dev-93</a>
+              </div>
+            </div>
+          </div>
+      
+          <div className="card skill">
+            <div>html / css</div>
+            <div>Javascript / React / Jquery</div>
+            <div>TypeScript</div>
+            <div>Git / GitHub</div>
           </div>
         </div>
       </div>
