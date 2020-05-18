ce8d6fb01ba4b8ad051332db6cfc56326b5fe0f0
dev-93 <cjb940103@gmail.com>
2020-05-19 00:09:20 +0900

    포트폴리오 컴포넌트화 및 swiper 추가

--- mypofo/src/pages/portfolio/Portfolio.js
+++ mypofo/src/pages/portfolio/Portfolio.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import "./portfolio.scss"
 import PortfolioList from "./PortfolioList";
+import Swiper from 'react-id-swiper';
+import 'swiper/css/swiper.css'
 
-const PortfolioItem = () => {
+const Portfolio = () => {
+  const params = {
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  }
   return (
     <>
-      {PortfolioList.map((list) => {
-        return (
-          <li>{list.id}</li>
-        );
-      })}
+      <div className="wrap_portfolio">
+        <Swiper {...params}>
+          {
+            PortfolioList.map((list,index)=>{
+              return (
+                <div className="cont" key={index}>
+                  <div className="explanation">
+                    <div className="container">
+                      <div className="title">{list.title}</div>
+                      <div className="skills">{list.skills}</div>
+                    </div>
+                  </div>
+                  
+                  <div className="picture">
+                    <figure>
+                      <img src={list.imgUrl} alt={`${list.title} 이미지`} />
+                    </figure>
+                  </div>
+                </div>
+              )
+            })
+          }
+          
+        </Swiper>
+      </div>
     </>
   );
 }
 
-const Portfolio = () => {
-  return (
-    <section className="portfolio">
-      <ul>
-        <PortfolioItem
-          key={PortfolioList.id}
-        />
-      </ul>
-    </section>
-  );
-}
-
 export default Portfolio;
+
+ 
