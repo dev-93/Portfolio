59259c0003b8fb72c8bddd9064870f60b8cd5469
dev-93 <cjb940103@gmail.com>
2020-05-19 00:07:22 +0900

    포트폴리오 리스트 수정

--- mypofo/src/pages/portfolio/PortfolioList.js
+++ mypofo/src/pages/portfolio/PortfolioList.js
@@ -1,27 +1,38 @@
 const PortfolioList = [{
   id: 1,
-  imgUrl: 2,
-  contents: 'qwrwqt',
+  imgUrl: 'https://http.cat/100',
+  title:"Moive Web",
+  skills:"REACT, react-router, Axios, Movie API",
 }, {
   id: 2,
-  imgUrl: 2,
-  contents: 'jft4',
+  imgUrl: 'https://http.cat/200',
+  title:"The Weather App",
+  skills:"REACT NATIVE, Weather API",
 }, {
   id: 3,
-  imgUrl: 2,
-  contents: 'jft4',
+  imgUrl: 'https://http.cat/300',
+  title:"쇼핑몰",
+  skills:"React",
 }, {
   id: 4,
-  imgUrl: 2,
-  contents: 'jft4',
+  imgUrl: 'https://http.cat/302',
+  title:"오늘의집",
+  skills:"html, css, jquery, 반응형 웹",
 }, {
   id: 5,
-  imgUrl: 2,
-  contents: 'jft4',
+  imgUrl: 'https://http.cat/303',
+  title:"풀무원",
+  skills:"html, css, jquery, 반응형 웹",
 }, {
   id: 6,
-  imgUrl: 2,
-  contents: 'jft4',
+  imgUrl: 'https://http.cat/400',
+  title:"탐앤탐스",
+  skills:"html, css, jquery, 반응형 웹",
+}, {
+  id: 7,
+  imgUrl: 'https://http.cat/404',
+  title:"마세라티",
+  skills:"html, css, jquery",
 }]
 
 export default PortfolioList;
