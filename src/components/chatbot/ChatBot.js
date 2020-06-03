6eee66354b834af5f10436f7d638fe9202a17f02
dev-93 <cjb940103@gmail.com>
2020-06-03 21:53:32 +0900

    chatbot js 코드정리

--- src/components/chatbot/ChatBot.js
+++ src/components/chatbot/ChatBot.js
@@ -1,25 +1,28 @@
-import React, { useState } from 'react';
-import "./chatbot.scss"
+import React, { useState } from "react";
+import "./chatbot.scss";
 import DetailBox from "./DetailBox";
 
 const Chatbot = (props) => {
-  const [isOpen, setOpen] = useState(false);
-  
-  return (
-    <>
-      <div className="wrap_chatbot">
-        <div className="container">
-          <button type="button" className="plus" onClick={()=>{setOpen(true)}}>+</button>
-          {
-            isOpen && 
-              <DetailBox 
-                setOpen={setOpen}
-              />
-          }
-        </div>
-      </div>
-    </>
-  );
-}
+    const [isOpen, setOpen] = useState(false);
+
+    return (
+        <>
+            <div className="wrap_chatbot">
+                <div className="container">
+                    <button
+                        type="button"
+                        className="plus"
+                        onClick={() => {
+                            setOpen(true);
+                        }}
+                    >
+                        +
+                    </button>
+                    {isOpen && <DetailBox setOpen={setOpen} />}
+                </div>
+            </div>
+        </>
+    );
+};
 
 export default Chatbot;
