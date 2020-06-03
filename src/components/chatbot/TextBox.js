24e5e1612bbbebd7c72e48f1676be1eb51be32ff
dev-93 <cjb940103@gmail.com>
2020-06-03 21:54:27 +0900

    textBox 코드정리

--- src/components/chatbot/TextBox.js
+++ src/components/chatbot/TextBox.js
@@ -1,46 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import chatList from "./chatList";
-import "./textbox.scss"
+import Answerbox from "./AnswerBox";
+import "./textbox.scss";
 
 const Textbox = (props) => {
-  
-  return (
-    <>
-      <div className="text_box next">
-        <div className="question">{chatList[props.num].question}</div>
-        <ul className="answer">
-          <li>
-            <button type="button" 
-              onClick={()=>{
-                props.setAnswer(true)
-                props.setTextBt(false)
-                }}>{chatList[props.num].answers.fir}</button>
-          </li>
-          <li>
-            <button type="button" 
-              onClick={()=>{
-                props.setAnswer(true)
-                props.setTextBt(false)
-                }}>{chatList[props.num].answers.sec}</button>
-          </li>
-          <li>
-            <button type="button" 
-              onClick={()=>{
-                props.setAnswer(true)
-                props.setTextBt(false)
-                }}>{chatList[props.num].answers.thi}</button>
-          </li>
-        </ul>
-      </div>
-
-      {
-        props.textBt && 
-        <div className="bt_box">
-          <button type="button" className="prev" onClick={()=>{props.setRecruit(false)}}>뒤로 돌아가기</button>    
-        </div>
-      }
-    </>
-  );
-}
+    const [answer, setAnswer] = useState(false);
+    return (
+        <>
+            <div className="text_box next">
+                <div className="question">{chatList[props.num].question}</div>
+                <ul className="answer">
+                    {chatList[props.num].answers.map((arr, index) => {
+                        return (
+                            <li key={index}>
+                                <button
+                                    type="button"
+                                    onClick={() => {
+                                        setAnswer(index + 4);
+                                        props.setTextBt(false);
+                                    }}
+                                >
+                                    {arr}
+                                </button>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
+            {(answer === 4 && (
+                <Answerbox
+                    setAnswer={props.setAnswer}
+                    setRecruit={props.setRecruit}
+                    setTextBt={props.setTextBt}
+                    textBt={props.textBt}
+                    num={4}
+                />
+            )) ||
+                (answer === 5 && (
+                    <Answerbox
+                        setAnswer={props.setAnswer}
+                        setRecruit={props.setRecruit}
+                        setTextBt={props.setTextBt}
+                        textBt={props.textBt}
+                        num={5}
+                    />
+                )) ||
+                (answer === 6 && (
+                    <Answerbox
+                        setAnswer={props.setAnswer}
+                        setRecruit={props.setRecruit}
+                        setTextBt={props.setTextBt}
+                        textBt={props.textBt}
+                        num={6}
+                    />
+                ))}
+            {props.textBt && (
+                <div className="bt_box">
+                    <button
+                        type="button"
+                        className="prev"
+                        onClick={() => {
+                            props.setRecruit(false);
+                        }}
+                    >
+                        뒤로 돌아가기
+                    </button>
+                </div>
+            )}
+        </>
+    );
+};
 
 export default Textbox;
