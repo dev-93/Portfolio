abc666781b90d906d9d2710e21284be597196847
dev-93 <cjb940103@gmail.com>
2020-06-03 21:52:24 +0900

    answerbox 코드정리

--- src/components/chatbot/AnswerBox.js
+++ src/components/chatbot/AnswerBox.js
@@ -1,35 +1,47 @@
-import React from 'react';
-import "./answerbox.scss"
-  
+import React from "react";
+import chatList from "./chatList";
+import "./answerbox.scss";
+
 const Answerbox = (props) => {
-  return (
-    <>
-      <div className="answer_box">
-        <ul>
-          <li>html</li>
-          <li>css</li>
-          <li>React</li>
-          <li>Javascript</li>
-          <li>Jquery</li>
-        </ul>
-      </div>
+    return (
+        <>
+            <div className="answer_box">
+                <ul>
+                    {chatList[props.num].answers.map((answer, index) => {
+                        return (
+                            <li key={index}>
+                                <div>{answer}</div>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </div>
 
-      <div className="answer_bt">
-        <button type="button" className="gohome" 
-          onClick={()=>{
-            props.setRecruit(false)
-            props.setAnswer(false)
-          }}>처음으로</button>
+            <div className="answer_bt">
+                <button
+                    type="button"
+                    className="gohome"
+                    onClick={() => {
+                        props.setRecruit(false);
+                        props.setAnswer(false);
+                    }}
+                >
+                    처음으로
+                </button>
 
-        <button type="button" className="prev" 
-          onClick={()=>{
-            props.setAnswer(false)
-            props.setTextBt(true)
-          }}>뒤로 돌아가기</button>    
-      </div>
-    </>
-  );
-}
-
+                <button
+                    type="button"
+                    className="prev"
+                    onClick={() => {
+                        props.setAnswer(false);
+                        props.setTextBt(true);
+                    }}
+                >
+                    뒤로 돌아가기
+                </button>
+            </div>
+        </>
+    );
+};
 
 export default Answerbox;
