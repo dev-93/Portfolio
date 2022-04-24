bf395d8c9d834eba62f5330362a498cc293f7bd5
kimtaenam <cjb940103@gmail.com>
2022-04-24 14:02:37 +0900

    파일 위치 변경

--- js/script.js
+++ js/script.js
@@ -4,6 +4,18 @@ textWrapper.innerHTML = textWrapper.textContent.replace(
   "<span class='letter'>$&</span>"
 );
 
+const about = document.querySelector("#about");
+const projects = document.querySelector("#projects");
+
+about.addEventListener("click", function () {
+  location.href =
+    "https://taenam.notion.site/Resume-2157e272e964476392fb8a99996e7c04";
+});
+
+projects.addEventListener("click", function () {
+  location.href = "./publishing/index.html";
+});
+
 anime.timeline().add({
   targets: ".title .letter",
   translateY: [100, 0],
